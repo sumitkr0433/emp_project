6951c81bf5c528e8d1385d803e641f74e89c1dd0
sumit kumar <50542078+sumitkr0433@users.noreply.github.com>
2022-08-17 10:03:46 +0530

    commented secret_key

--- rajesh/projects/settings.py
+++ rajesh/projects/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sdxc*)6n!m1jk_i2cdf^69%m8ae&u*9k-2wj3cudmmz%07_9!4'
+#SECRET_KEY = 'django-insecure-sdxc*)6n!m1jk_i2cdf^69%m8ae&u*9k-2wj3cudmmz%07_9!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
